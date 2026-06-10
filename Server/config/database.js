c57cb99fb9ba72e94bdcd3b24e3c2568669e7d5a
phu142857 <nguyentaiphu980@gmail.com>
2026-06-10 21:33:39 +0700

    reduce polling lag

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const { serverConfig } = require('./serverConfig');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(serverConfig.database.uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
